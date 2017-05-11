e79187aeb51741e2d5fd1a577597461aed2a6620
Dakota Ling <ourthinking@gmail.com>
2017-05-10 23:52:22 -0400

    Week 6 slides, updated demo

--- demos/backgrounds/assets/js/main.js
+++ demos/backgrounds/assets/js/main.js
@@ -1,11 +1,30 @@
-$(document).ready(function(){
+$(function(){
 
-	$('.big-image').on('click', function(){
-		$(this).addClass('loaded');
-	});
+	// simulate image loading
+	setTimeout(function(){
+		$('.big-image').addClass('loaded');
+	}, 1000);
 
 	$('.menu-button').on('click', function(){
 		$('nav').toggleClass('show');
-	})
+	});
+
+	$('.inline-ad').parallax({
+		imageSrc: 'http://lorempixel.com/output/nightlife-q-c-1280-720-4.jpg',
+		speed: 0.5
+	});
+
+	$('.author-image').on('click', function(){
+		$.scrollTo(0, 500);
+	});
+
+	$('.preamble').waypoint(function(){
+		$(this.element).children('blockquote').addClass('open');
+		// force parallax plugin to resize after this opens
+		setTimeout(function(){
+			$(window).trigger('resize');
+		}, 300);
+		
+	});
 
 });
